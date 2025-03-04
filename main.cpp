146014ad066eaa9f286876223352e7da987e3305
VictorAJM <victorajm@outlook.com>
2025-03-04 14:31:36 -0600

    a

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main() {
-  cin.tie(0)->ios_base::sync_with_stdio(0);
-  int n;
-  cin >> n;
-  for (int i=1;i<=n;i++) cout << i<<"\n";
+  int a[2000];
+  cin >> a[0];
   return 0;
 }
