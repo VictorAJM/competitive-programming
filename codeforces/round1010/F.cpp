d4cd5c2feb5dac43b6b18af6336075ba3639e0e9
VictorAJM <victorajm@outlook.com>
2025-03-18 12:30:31 -0600

    ronda 1010

---
+++ codeforces/round1010/F.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+using namespace std;
+
+const int mod;
+using ii = pair<int,int>;
+using ll = long long;
+
+void solve() {
+
+}
+int main() {
+  cin.tie(0)->ios_base::sync_with_stdio(0);
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
